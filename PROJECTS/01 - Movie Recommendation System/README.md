f2ca9996bb930555eda189017f6464910289ef29
aryan223 chugh <chugharyan816@gmail.com>
2020-07-14 14:34:36 +0530

    Added essential files for heroku

---
+++ PROJECTS/01 - Movie Recommendation System/README.md
@@ -0,0 +1,13 @@
+# **Movie Recommendation System**
+
+### This is a simple project that uses basic linear algebra and libraries like Numpy, pandas and seaborn to create a **content-based recommender system**.<br>There is a python notebook for **explanation of the applied concepts used for building this project from scratch**, please [click here](<https://github.com/aryanchugh816/Data-Science/blob/master/PROJECTS/01%20-%20Movie%20Recommendation%20System/Movie%20Recommendation%20System%20(%20Content%20Based%20).ipynb>) to visit it.<br><br>This project is built using Streamlit and hosted on Heroku, you can see the ["_index.py_"](https://github.com/aryanchugh816/Data-Science/blob/master/PROJECTS/01%20-%20Movie%20Recommendation%20System/index.py) file for the source code.<br><br>_Link to an article explaining this whole code and concept will be uploaded soon._
+
+## **Highlights:**
+
+1. Full movies database can be viewed at a glance on the home page<br><br>
+2. Selection of movie using a drop down list that integrates smart regex to find your movie as you type movie name<br>
+   ![Movie Selection](Pictures/selecting_movie.gif)<br><br>
+3. Visualizing data related to the movie with easy to read recommendations<br><br>
+   ![Visualizing Data](Pictures/visualizing_data.gif)
+
+### If you like any of my work please feel free to contact me related to any project collaboration or job opportunity
--- PROJECTS/01 - Movie Recommendation System/index.py
+++ PROJECTS/01 - Movie Recommendation System/index.py
@@ -36,37 +36,45 @@ def predict_movies(movie_name, titles):
 
     return predictions
 
-st.title("Movie Recommendation System")
-st.subheader("This is a simple application of a content based recommendation system using just pandas and simple linear algebra concepts")
-st.subheader(" ")
-st.markdown("[Click here]() to see the code and feel free to browse my [Github repository](https://github.com/aryanchugh816/Data-Science) for more projects and articles related to machine learning")
-st.subheader(" ")
-st.subheader("This is the Movies Dataset, please feel free to browse it")
-
 # Loading dataframes
 movie_titles = load_data("Datasets/Movie_title_rating.csv")
 movie_matrix = load_data("Datasets/Movie_matrix.csv")
-st.dataframe(movie_titles)
 
-movie_name = st.selectbox("Which movie have you just watched ? (Start typing...)", ([''] + list(movie_titles.iloc[:,0])))
-if movie_name != '':
-    st.subheader("Movie selected: " + movie_name)
-    graph_option = st.radio("Visualize data of the selected movie", ("No Visualization", "Rating Distribution", "Rating Distribution differentiated by gender"))
-    if graph_option == "Rating Distribution":
-        sns.distplot(movie_matrix[movie_name], bins=10)
-        plt.xlabel("Rating (Out of 5.0)")
-        st.pyplot()
+def main():
+    st.title("Movie Recommendation System")
+    st.subheader("This is a simple application of a content based recommendation system using just pandas and simple linear algebra concepts")
+    st.subheader(" ")
+    st.markdown("[Click here](https://github.com/aryanchugh816/Data-Science/blob/master/PROJECTS/01%20-%20Movie%20Recommendation%20System/index.py) to see the code and feel free to browse my [Github repository](https://github.com/aryanchugh816/Data-Science) for more projects and articles related to machine learning")
+    st.subheader(" ")
+    st.subheader("This is the Movies Dataset, please feel free to browse it")
 
-    elif graph_option == "Rating Distribution differentiated by gender":
-        temp_df = pd.read_csv("Datasets/u.user", names=["user id", "age", "gender", "occupation", "zip code"], sep="\|")
-        temp_df.drop(columns=["age", "occupation", "zip code", "user id"], inplace = True)
-        temp_df["rating"] = movie_matrix[movie_name]
-        sns.countplot(y="rating", data=temp_df, hue="gender", palette="Accent")
-        st.pyplot()
+    # Loading dataframes
+    # movie_titles = load_data("Datasets/Movie_title_rating.csv")
+    # movie_matrix = load_data("Datasets/Movie_matrix.csv")
+    # st.dataframe(movie_titles)
 
-    st.subheader("Recommended Movies:")
-    predicted_movies = predict_movies(movie_name, movie_titles)
-    st.dataframe(predicted_movies)
+    movie_name = st.selectbox("Which movie have you just watched ? (Start typing...)", ([''] + list(movie_titles.iloc[:,0])))
+    if movie_name != '':
+        st.subheader("Movie selected: " + movie_name)
+        graph_option = st.radio("Visualize data of the selected movie", ("No Visualization", "Rating Distribution", "Rating Distribution differentiated by gender"))
+        if graph_option == "Rating Distribution":
+            sns.distplot(movie_matrix[movie_name], bins=10)
+            plt.xlabel("Rating (Out of 5.0)")
+            st.pyplot()
+
+        elif graph_option == "Rating Distribution differentiated by gender":
+            temp_df = pd.read_csv("Datasets/u.user", names=["user id", "age", "gender", "occupation", "zip code"], sep="\|")
+            temp_df.drop(columns=["age", "occupation", "zip code", "user id"], inplace = True)
+            temp_df["rating"] = movie_matrix[movie_name]
+            sns.countplot(y="rating", data=temp_df, hue="gender", palette="Accent")
+            st.pyplot()
+
+        st.subheader("Recommended Movies:")
+        predicted_movies = predict_movies(movie_name, movie_titles)
+        st.dataframe(predicted_movies)
+
+if __name__ == "__main__":
+    main()
 
 
 
